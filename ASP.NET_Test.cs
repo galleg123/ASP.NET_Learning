9c30f849776435f68c0430c84baceda7a66f6867
Mikkel Silvasen <mikkelkp360@gmail.com>
2023-11-17 12:15:05 +0100

    Intitial commit, with only two numbers as input

---
+++ ASP.NET_Test.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Example usage of the AddNumbers function
+        int result = AddNumbers(5, 7);
+        Console.WriteLine("The sum is: " + result);
+
+        int result2 = MultiplyNumbers(5, 7);
+        Console.WriteLine("The sum is: " + result);
+    }
+
+    // Definition of the AddNumbers function
+    static int AddNumbers(int num1, int num2)
+    {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    static int MultiplyNumbers(int num1, int num2)
+    {
+        int product = num1 * num2;
+        return product
+    }
+}
